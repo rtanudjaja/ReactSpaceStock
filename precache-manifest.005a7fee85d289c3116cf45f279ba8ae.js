855b3a8af9efc494da84106df05a79947a3e2d44
Ricky Tanudjaja <rickytanudjaja95@gmail.com>
2020-07-01 02:28:30 +1000

    First push

---
+++ precache-manifest.005a7fee85d289c3116cf45f279ba8ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4442ce07b9d855209291f70ffa0a4f74",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca2c81c1d85a957da9dd",
+    "url": "/static/css/main.0f86a802.chunk.css"
+  },
+  {
+    "revision": "57e3b70db6dfd488bbdf",
+    "url": "/static/js/2.bb273ce0.chunk.js"
+  },
+  {
+    "revision": "54fd6816419f7824e2804745930c9117",
+    "url": "/static/js/2.bb273ce0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca2c81c1d85a957da9dd",
+    "url": "/static/js/main.bedf6789.chunk.js"
+  },
+  {
+    "revision": "15137351e525f934eca2",
+    "url": "/static/js/runtime-main.7408996a.js"
+  }
+]);
